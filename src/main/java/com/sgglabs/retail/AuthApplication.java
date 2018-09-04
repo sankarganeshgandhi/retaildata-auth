c2d2b7bbb352539d8b3d3e6ff4cf39639cf7940b
Sankarganesh Gandhi <sankarganesh.gandhi@happiestminds.com>
2018-09-04 16:19:27 +0100

    Initial draft implementation for auth module for retaildata projects

--- src/main/java/com/sgglabs/retail/AuthApplication.java
+++ src/main/java/com/sgglabs/retail/AuthApplication.java
@@ -1,13 +1,19 @@
 package com.sgglabs.retail;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * Hello world!
+ * @author Sankarganesh (sgandhi@sgglabs.com)
  *
  */
-public class AuthApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class AuthApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
